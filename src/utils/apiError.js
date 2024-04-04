7031f34c0e44f030259337d49c11029efd6299ee
akshat <akshatbhatya@gmail.com>
2024-04-04 09:36:04 +0530

    api error is done

---
+++ src/utils/apiError.js
@@ -0,0 +1,15 @@
+class ApiError extends Error{
+    constructor(status,message,error,data,stack){
+        super(message)
+        this.stack=stack,
+        this.message=message,
+        this.error=error,
+        this.status=status
+
+        if(!stack){
+           this.stack= Error.captureStackTrace(this,this.constructor);
+        }
+    }
+}
+
+export default ApiError;
