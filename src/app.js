c34bf07cc7c25a181f3ba9bee51ef49204fb9ccf
akshat <akshatbhatya@gmail.com>
2024-04-04 09:12:18 +0530

    express and midlewares configuratin is done

--- src/app.js
+++ src/app.js
@@ -1,4 +1,18 @@
-import  Express  from "express";
+import  Express, { json, urlencoded }  from "express";
+import cors from "cors"
 const app=Express();
+//  middleware
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+//  add middlewares for json file accept 16 kb 
+app.use(json({limit:"16kb"}))
+
+app.use(urlencoded({
+    limit:"16kb",
+    extended:true}))
 
 export default app;
