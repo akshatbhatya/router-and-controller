3f573252aa81677f4f827677b82dc38438989253
akshat <akshatbhatya@gmail.com>
2024-04-04 12:18:48 +0530

    async function is done

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+const asyncHandller=(requestHandler)=>{
+    return (req,res,next)=>{
+         Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+        Promise.reject("function is rejected")
+
+    }
+
+}
+
+
+
+// function asyncHAndler(fun){
+
+//    return async(req,res,next)=>{
+//     try {
+//         await fun(req,res,next)
+//         next()
+//     } catch (error) {
+//         res.status(err.code|| 500).json({
+//             sucess:false,
+//             message:error.message,
+//             status:500
+//         })
+//         console.log(error);    
+//     }
+//     }
+
+//  }
+
+export default asyncHandller;
