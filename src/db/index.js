a36a9d79f31f679c6f4817ff25422977c8cfc17d
akshat <akshatbhatya@gmail.com>
2024-04-04 09:23:15 +0530

    db file is done

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import  databaseName  from "../constants";
+
+const connectDb=async()=>{
+       try {
+        
+       const dataResponse= await mongoose.connect(`${process.env.MONGODB_URI}/${databaseName}`);
+       console.log(dataResponse.connection.host);
+       return dataResponse.connection.host
+       } catch (error) {
+        console.log(error,"error is occured in db connection");
+        
+       }
+    
+}
+
+export default connectDb;
